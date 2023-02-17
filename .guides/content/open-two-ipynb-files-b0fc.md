57815668e77fec2ca80086ce5955f867dbe95025
Igor Yashtykov <iyashtykov@codio.com>
2023-02-17 12:14:31 +0000

    init

---
+++ .guides/content/open-two-ipynb-files-b0fc.md
@@ -0,0 +1,1 @@
+open two ipynb files
---
+++ nbgrader_config.py
@@ -0,0 +1,14 @@
+c = get_config()
+c.ClearHiddenTests.begin_test_delimeter = "THIS IS SPARTA!"
+c.ClearHiddenTests.end_test_delimeter = "UPS... THIS IS NOT SPARTA"
+c.LockCells.lock_all_cells = True
+c.LockCells.lock_grade_cells = True
+c.LockCells.lock_readonly_cells = True
+c.LockCells.lock_solution_cells = True
+c.ExecutePreprocessor.interrupt_on_timeout = True
+c.ExecutePreprocessor.timeout = 20
+c.ClearSolutions.code_stub = {
+"R": "# your R code here\n# end of R code\n",
+"python": "# your python code here\n# end of python code\n",
+"ruby": "# your ruby code here            \n# end of ruby code"
+}
