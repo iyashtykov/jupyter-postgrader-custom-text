b0d559eced8f67130593215e4e6c5523e23fcf86
Igor Yashtykov <iyashtykov@users.noreply.github.com>
2024-05-15 15:21:08 +0100

    bigger timeout

--- nbgrader_config.py
+++ nbgrader_config.py
@@ -6,7 +6,7 @@ c.LockCells.lock_grade_cells = True
 c.LockCells.lock_readonly_cells = True
 c.LockCells.lock_solution_cells = True
 c.ExecutePreprocessor.interrupt_on_timeout = True
-c.ExecutePreprocessor.timeout = 20
+c.ExecutePreprocessor.timeout = 300
 c.ClearSolutions.code_stub = {
 "R": "# your R code here\n# end of R code\n",
 "python": "# your python code here\n# end of python code\n",
